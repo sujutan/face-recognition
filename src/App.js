0965c4aaeed162b7a8f4c2bbfe7128f10be38ae7
Suju Tan <sujuktan@gmail.com>
2023-03-17 11:45:28 -0400

    Added particles at the background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import Background from "./components/background/Background";
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
+import Rank from "./components/rank/Rank";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 
 //styles
@@ -8,8 +10,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Background />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
