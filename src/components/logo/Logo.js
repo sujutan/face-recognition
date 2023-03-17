5a6b4244ddc409aa96d004108efdce07c4f216e6
Suju Tan <sujuktan@gmail.com>
2023-03-16 23:27:21 -0400

    added the logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,5 +1,19 @@
 import React from "react";
 
+import Tilt from "react-parallax-tilt";
+import face from "../../images/face.png";
+
+//styles
+import "./Logo.css";
+
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div className="ma4 mt0">
+      <Tilt className="parallax-effect" perspective={500}>
+        <div className="inner-element">
+          <img src={face} alt="face logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
 }
